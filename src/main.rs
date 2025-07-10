129f708079cb47f27c2d69587c6b6d53606357d3
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2025-07-10 21:38:01 +0530

    chore: simple libp2p implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use futures::stream::StreamExt;
+use libp2p::{
+    SwarmBuilder,
+    kad::{self, Mode, store::MemoryStore},
+    mdns, noise,
+    swarm::{NetworkBehaviour, SwarmEvent},
+    tcp, yamux,
+};
+use tokio::{
+    io::{self, AsyncBufReadExt},
+    select,
+};
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init();
+
+    // We create a custom network behaviour that combines Kademlia and mDNS.
+    #[derive(NetworkBehaviour)]
+    struct Behaviour {
+        kademlia: kad::Behaviour<MemoryStore>,
+        mdns: mdns::tokio::Behaviour,
+    }
+
+    let mut swarm = SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|key| {
+            Ok(Behaviour {
+                kademlia: kad::Behaviour::new(
+                    key.public().to_peer_id(),
+                    MemoryStore::new(key.public().to_peer_id()),
+                ),
+                mdns: mdns::tokio::Behaviour::new(
+                    mdns::Config::default(),
+                    key.public().to_peer_id(),
+                )?,
+            })
+        })?
+        .build();
+
+    swarm.behaviour_mut().kademlia.set_mode(Some(Mode::Server));
+
+    // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+
+    // Listen on all interfaces and whatever port the OS assigns.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Kick it off.
+    loop {
+        select! {
+        Ok(Some(line)) = stdin.next_line() => {
+
+        }
+        event = swarm.select_next_some() => match event {
+            SwarmEvent::NewListenAddr { address, .. } => {
+                println!("Listening in {address:?}");
+            },
+            SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                for (peer_id, multiaddr) in list {
+                    swarm.behaviour_mut().kademlia.add_address(&peer_id, multiaddr);
+                }
+            },
+            _ => {}
+        }
+        }
+    }
 }
