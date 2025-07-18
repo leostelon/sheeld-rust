517680a640651097ff96137588e373c13a2d4bd1
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2025-07-18 14:56:22 +0530

    chore: util to spawn and handle tokio futures

---
+++ src/common/utils/spawn_and_log_error.rs
@@ -0,0 +1,14 @@
+use fast_socks5::Result;
+use tokio::task;
+
+pub fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+where
+    F: Future<Output = Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        match fut.await {
+            Ok(()) => {}
+            Err(err) => println!("{:#}", &err),
+        }
+    })
+}
